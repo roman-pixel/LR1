e42ae6d498ecd1834e15a7fbe3d1f9cf3c528781
sadovrs <sadovrs@ARMCSR17>
2022-09-12 13:08:00 +0300

    небольшие правки

--- js/ui.js
+++ js/ui.js
@@ -1,26 +1,28 @@
 const recipes = document.querySelector('.recipes');
 
 const renderRecipe = (data, id) => {
-    const html = `
+  const html = `
         <div class="card-panel recipe white row" data-id="${id}">
             <img src="/img/dish.png" alt="recipe thumb" />
             <div class="recipe-details">
                 <div class="recipe-title">${data.title}</div>
-                <div class="recipe-ingredients">${data.ingredients}</div>
-                <div class="recipe-price">${data.price}</div>
+                <div class="recipe-ingredients">${data.ingredients}</div>                              
+            </div>
+            <div class="recipe-price">
+                <div class="recipe-price">${data.price} &#8381</div>
             </div>
             <div class="recipe-delete">
                 <i class="material-icons" data-id="${id}">delete_outline</i>
             </div>
         </div>`;
-    recipes.innerHTML += html;
+  recipes.innerHTML += html;
 };
 
 document.addEventListener('DOMContentLoaded', function () {
-    const menus = document.querySelectorAll('.side-menu');
-    M.Sidenav.init(menus, { edge: 'right' });
-    const forms = document.querySelectorAll('.side-form');
-    M.Sidenav.init(forms, { edge: 'left' });
+  const menus = document.querySelectorAll('.side-menu');
+  M.Sidenav.init(menus, { edge: 'right' });
+  const forms = document.querySelectorAll('.side-form');
+  M.Sidenav.init(forms, { edge: 'left' });
 });
 
 document.querySelectorAll('input').forEach((input) => {
@@ -31,7 +33,7 @@ document.querySelectorAll('input').forEach((input) => {
 });
 
 const removeRecipe = (id) => {
-    const recipe = document.querySelector(`.recipe[data-id=${id}]`);
-    recipe.remove();
+  const recipe = document.querySelector(`.recipe[data-id=${id}]`);
+  recipe.remove();
 };
 
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const staticCacheName = 'site-static-v1';
+const staticCacheName = 'site-static-v2';
 const dynamicCacheName = 'site-dynamic-v1';
 
 const assets = [
